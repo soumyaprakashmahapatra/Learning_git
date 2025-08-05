3cc0252c7bb8a6c34e21dac852b81020c1887fe8
SOUMYA PRAKASH MAHAPATRA <soumyaprakashmahapatra@SOUMYAs-MacBook-Air.local>
2025-08-05 15:53:59 +0530

    add something to hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,7 @@
 class Hello{
     public static void main(String[] args) {
         System.out.println("Hello world");
+        //hey how are you
+        System.out.println("Hey how r u");
     }
 }
